61c99f096ea0a1fc67f100aab7d6349f3e25819d
Juan Camilo Marin <camilo@eulerr.app>
2021-05-03 22:05:41 -0500

    Init

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,43 +1,72 @@
 <template>
   <div id="container">
-    <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <video ref="videoPlayer" class="video-js"></video>   
   </div>
 </template>
 
-<script lang="ts">
 
-export default {
-  name: 'ExploreContainer',
-  props: {
-    name: String
+<script>
+  import videojs from 'video.js';
+  import { registerIVSTech } from 'amazon-ivs-player';
+  import 'video.js/dist/video-js.css';
+
+
+  export default {
+    name: 'ExploreContainer',
+    
+    data: () => ({
+      player: null,
+      videoSource: process.env.VUE_APP_CHAN_ENDPOINT,
+      videoOptions: {
+        autoplay: false,
+        controls: true,
+        techOrder: ["AmazonIVS"],
+        width: "800"
+      }
+    }),
+
+    mounted() {
+      // register the tech with videojs
+      console.log(`wasmWorker: ${this.createAbsolutePath('/assets/amazon-ivs-wasmworker.min.js')}`);
+
+      registerIVSTech(videojs,  {
+        wasmWorker: this.createAbsolutePath('/assets/amazon-ivs-wasmworker.min.js'),
+        wasmBinary: this.createAbsolutePath('/assets/amazon-ivs-wasmworker.min.wasm'),
+      });
+
+      // Init the player
+      this.player = videojs(this.$refs.videoPlayer, this.videoOptions, () => {
+        console.log('Player is ready to use!');
+        // play the stream
+        this.player.src(this.videoSource);
+      })
+    },
+
+    beforeUnmount() {
+      // Destroy the player instance
+      if(this.player) {
+        this.player.dispose();
+      }
+    },
+
+    methods: {
+      createAbsolutePath(assetPath) {
+        return new URL(assetPath, document.URL).toString();
+      }
+    }
   }
-}
 </script>
 
 <style scoped>
 #container {
+  display: flex;
+  justify-content: center;
+  align-content: center;
   text-align: center;
   position: absolute;
   left: 0;
   right: 0;
   top: 50%;
   transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
 }
 </style>
